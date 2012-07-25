dad7ea3453d7bc6a7462e5968d2a92d992f1dd0d
Ric Allinson <capecodehq@yahoo.co.uk>
2012-07-25 14:51:20 -0700

    Minor updates

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+/*global describe: true, it: true*/
+
+'use strict';
+
+var assert = require("assert");
+
+describe('ash', function () {
+    describe('nothing', function () {
+        it('should have listen to my mum', function () {
+            assert.equal(false);
+        });
+    });
+});
