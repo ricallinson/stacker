4da5a58f7ad50f4fc305d185f92823a532cabed8
Ric Allinson <capecodehq@yahoo.co.uk>
2012-07-25 14:40:48 -0700

    Updated example

--- examples/simple/app.js
+++ examples/simple/app.js
@@ -52,7 +52,7 @@ app.get('/', function (req, res) {
     html = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"> \
         <html> \
           <head> \
-            <title>Ash Knot Test</title> \
+            <title>Stacker Simple Example</title> \
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> \
             <script type="text/javascript" src="/knot/process.js"></script> \
             <script type="text/javascript"> \
